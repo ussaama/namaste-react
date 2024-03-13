97420af18ab78fbf4e40c49bfa0577cafd591219
Muhammad Usama <usamazafar543@gmail.com>
2024-03-13 23:24:53 +0400

    episode-1

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am an h1 tag"),
+    React.createElement("h2", {}, "i am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am an h1 tag"),
+    React.createElement("h2", {}, "i am an h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(parent);
